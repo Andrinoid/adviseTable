aff333850c4dd17828c5cbd4d625007e70c64e87
Andri <andri@advise.is>
2022-12-21 14:30:49 +0000

    Update index.js

--- src/Table/Col/index.js
+++ src/Table/Col/index.js
@@ -37,7 +37,6 @@ let Outliner = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    // border: 1px solid transparent;
     pointer-events: none;
     z-index: 100;
     &.hightlighted {
