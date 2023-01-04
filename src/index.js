a7a43439cc560fe59e3748ed7868d30dfcb87f6a
FranklynKleiton <franklynkleiton18@gmail.com>
2023-01-04 14:47:43 -0300

    Update exports.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import { Table, Row, Col } from "./Table";
-import { view, ui_prefs } from "./data/example2";
-import { default as mo } from "./data/months";
-import { default as DragHandle } from "./icons/DragHandle";
-import { default as Plus } from "./icons/Plus";
-
-export default {
-  Example,
-  Table,
-  Row,
-  Col,
-  view,
-  ui_prefs,
-  mo,
-  DragHandle,
-  Plus,
-};
+export { Table, Row, Col } from "./Table";
+export { view, ui_prefs } from "./data/example2";
+export { default as mo } from "./data/months";
+export { default as DragHandle } from "./icons/DragHandle";
+export { default as Plus } from "./icons/Plus";
