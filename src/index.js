7dc5f7b56e19703e5df0001ea900e69ac58cffc4
FranklynKleiton <franklynkleiton18@gmail.com>
2023-01-04 11:13:41 -0300

    Add exports.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 export { default as Example } from "./Example";
+
+export { Layout, theme, Radio, Typography, Switch, Button, Slider } from "antd";
+
+export { Table, Row, Col } from "./Table";
+export { view, ui_prefs } from "./data/example2";
+export { default as mo } from "./data/months";
+export { default as DragHandle } from "./icons/DragHandle";
+export { default as Plus } from "./icons/Plus";
