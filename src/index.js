2db352011cc38edad0a20805e625973c83046597
Franklyn <franklynkleiton18@gmail.com>
2023-01-08 13:16:30 -0300

    Update export.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
-export { Table, Row, Col } from "./Table";
-export { view, ui_prefs } from "./data/example2";
-export { default as mo } from "./data/months";
-export { default as DragHandle } from "./icons/DragHandle";
-export { default as Plus } from "./icons/Plus";
+export { Table, Row, Col, Header } from "./Table";
+
