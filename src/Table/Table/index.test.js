9355d16356667cb4c72ba1b6b96819c64fb5523d
Franklyn <franklynkleiton18@gmail.com>
2023-02-04 23:08:06 -0300

    Add comment.

--- src/Table/Table/index.test.js
+++ src/Table/Table/index.test.js
@@ -32,6 +32,10 @@ describe("Copier", () => {
     });
 
     it("should return selection string from row", () => {
+      // -- x x x x x x x x x x x x x
+      // oo o o o o o o o o o o o o o
+      // -- x x x x x x x x x x x x x
+
       const selection = [
         {
           fromX: 0,
@@ -48,39 +52,39 @@ describe("Copier", () => {
         "Gamalt ekki notað\t\t\t\t\t\t-146580\t-146580\t-155772\t-146580\t-146580\t\t\t34567\n"
       );
     });
+  });
 
-    describe("Multiple Selections", () => {
-      it("should return selection string from multiple selections", () => {
-        // -- x x x x x x x
-        // -- o o x o o x x
-        // -- o o x o o x x
-        // -- o o x o o x x
-        // -- o o x o o x x
-        const selection = [
-          {
-            fromX: 1,
-            fromY: 5,
-            toX: 2,
-            toY: 7,
-            oldMouseMoveTo: { toX: 2, toY: 7 },
-            isExclusion: false,
-          },
-          {
-            fromX: 4,
-            fromY: 5,
-            toX: 5,
-            toY: 7,
-            oldMouseMoveTo: { toX: 5, toY: 7 },
-            isExclusion: false,
-          },
-        ];
+  describe("Multiple Selections", () => {
+    it("should return selection string from multiple selections", () => {
+      // -- x x x x x x x
+      // -- o o x o o x x
+      // -- o o x o o x x
+      // -- o o x o o x x
+      // -- o o x o o x x
+      const selection = [
+        {
+          fromX: 1,
+          fromY: 5,
+          toX: 2,
+          toY: 7,
+          oldMouseMoveTo: { toX: 2, toY: 7 },
+          isExclusion: false,
+        },
+        {
+          fromX: 4,
+          fromY: 5,
+          toX: 5,
+          toY: 7,
+          oldMouseMoveTo: { toX: 5, toY: 7 },
+          isExclusion: false,
+        },
+      ];
 
-        const copier = new Copier(table, selection);
+      const copier = new Copier(table, selection);
 
-        expect(copier.stringifyTable()).toBe(
-          `-1079711\t-1022849\t \t-2991679\t144949\n-4715000\t-4715000\t \t-4715000\t-4715000\n39284029\t35977158\t \t30156737\t38093535\n`
-        );
-      });
+      expect(copier.stringifyTable()).toBe(
+        `-1079711\t-1022849\t \t-2991679\t144949\n-4715000\t-4715000\t \t-4715000\t-4715000\n39284029\t35977158\t \t30156737\t38093535\n`
+      );
     });
   });
 });
