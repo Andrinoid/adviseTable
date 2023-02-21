9444eb2381805f3da5b7ced9183bfde3569143de
Franklyn <franklynkleiton18@gmail.com>
2023-02-20 22:33:56 -0300

    WIP: allow user to select using shift + keydirection

--- src/Table/Table/KeyboardSelection.js
+++ src/Table/Table/KeyboardSelection.js
@@ -8,6 +8,16 @@ export default function useKeyboardSelection(
   let isNegative = useRef(false);
 
   useEffect(() => {
+    function updateLabelArea(area, row, col) {
+      const element = tableMatrix[row][col].current;
+      const colspan = element.getAttribute("data-colspan");
+      const x = element.getAttribute("data-x");
+      if (colspan && x) {
+        area.fromX = parseInt(x);
+        area.toX = parseInt(x) + parseInt(colspan) - 1;
+      }
+    }
+
     function handleSelectionKey(event) {
       const area = selectedAreas[0];
       const lastCol = tableMatrix[0].length - 1;
@@ -16,55 +26,54 @@ export default function useKeyboardSelection(
       if (event.shiftKey) {
         if (event.key === "ArrowDown") {
           if (area.toY < lastRow) {
-            if (area.toY === area.fromY) {
-              isNegative.current = false;
-            }
-            if (!isNegative.current) area.toY += 1;
-            else {
+            updateLabelArea(area, area.fromY + 1, area.fromX);
+          }
+
+          if (area.toY === area.fromY) {
+            isNegative.current = false;
+          }
+          if (!isNegative.current) area.toY += 1;
+          else {
+            if (area.toY < lastRow) {
               area.fromY += 1;
             }
           }
         }
 
         if (event.key === "ArrowUp") {
-          // const element = tableMatrix[area.fromY-1][area.fromX].current;
-          // const colspan = element.getAttribute("data-colspan");
-          // const x = element.getAttribute("data-x");
-          // if (colspan) {
-          //   area.fromX = parseInt(x);
-          //   area.toX = parseInt(x) + parseInt(colspan)-1;
-          // }
-          // console.log(area)
           if (area.fromY > 0) {
-            if (area.toY === area.fromY) {
-              isNegative.current = true;
-            }
-            if (!isNegative.current) area.toY -= 1;
-            else {
+            updateLabelArea(area, area.fromY - 1, area.fromX);
+          }
+          if (area.toY === area.fromY) {
+            isNegative.current = true;
+          }
+          if (!isNegative.current) area.toY -= 1;
+          else {
+            if (area.fromY > 0) {
               area.fromY -= 1;
             }
           }
         }
 
         if (event.key === "ArrowLeft") {
-          if (area.fromX > 0) {
-            if (area.toX === area.fromX) {
-              isNegative.current = true;
-            }
-            if (isNegative.current) area.fromX -= 1;
-            else {
+          if (area.toX === area.fromX) {
+            isNegative.current = true;
+          }
+          if (isNegative.current) area.fromX -= 1;
+          else {
+            if (area.fromX > 0) {
               area.toX -= 1;
             }
           }
         }
 
         if (event.key === "ArrowRight") {
-          if (area.toX < lastCol) {
-            if (area.toX === area.fromX) {
-              isNegative.current = false;
-            }
-            if (isNegative.current) area.fromX += 1;
-            else {
+          if (area.toX === area.fromX) {
+            isNegative.current = false;
+          }
+          if (isNegative.current) area.fromX += 1;
+          else {
+            if (area.toX < lastCol) {
               area.toX += 1;
             }
           }
