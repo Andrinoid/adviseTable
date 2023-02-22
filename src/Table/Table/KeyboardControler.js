1afc6236fff2c615083c1f42889e58ba02872a64
Andri <andri@advise.is>
2023-02-22 00:02:30 +0000

    refactor KeyboardController to use Hotkeys

--- src/Table/Table/KeyboardControler.js
+++ src/Table/Table/KeyboardControler.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { useHotkeys } from 'react-hotkeys-hook'
 
 export default function useKeyboardControler(
   selectedAreas,
@@ -6,102 +7,108 @@ export default function useKeyboardControler(
   setSelectedAreas
 ) {
   let isNegative = useRef(false);
+  useHotkeys('up', () => arrowMoveSelection('up', false));
+  useHotkeys('down', () => arrowMoveSelection('down', false));
+  useHotkeys('left', () => arrowMoveSelection('left', false));
+  useHotkeys('right', () => arrowMoveSelection('right', false));
 
-  useEffect(() => {
-    function handleSelectionKey(event) {
-      event.preventDefault();
-      const area = selectedAreas[selectedAreas.length - 1];
-      const lastCol = tableMatrix[selectedAreas.length - 1].length - 1;
-      const lastRow = tableMatrix.length - 1;
+  useHotkeys('shift+up', () => arrowShiftSelection('up'));
+  useHotkeys('shift+down', () => arrowShiftSelection('down'));
+  useHotkeys('shift+left', () => arrowShiftSelection('left'));
+  useHotkeys('shift+right', () => arrowShiftSelection('right'));
 
-      if (event.shiftKey) {
-        if (event.key === "ArrowDown") {
-          if (area.toY < lastRow) {
-            updateLabelArea(area, area.fromY + 1, area.fromX, tableMatrix);
-          }
+  const arrowMoveSelection = (keyName) => {
+    const area = selectedAreas[selectedAreas.length - 1];
 
-          if (area.toY === area.fromY) {
-            isNegative.current = false;
-          }
-          if (area.toY < lastRow) {
-            if (!isNegative.current) area.toY += 1;
-            else {
-              area.fromY += 1;
-            }
-          }
-        }
+    if (keyName === "right") {
+      // move selected up to the next row but not past the last row
+      area.toX = Math.min(tableMatrix[area.toY].length - 1, area.toX + 1);
+      area.fromX = area.toX;
+      area.fromY = area.toY;
+    }
+    if (keyName === "left") {
+      area.toX = Math.max(0, area.toX - 1);
+      console.log(area.toX);
+      area.fromX = area.toX;
+      area.fromY = area.toY;
+    }
+    if (keyName === "up") {
 
-        if (event.key === "ArrowUp") {
-          if (area.fromY > 0) {
-            updateLabelArea(area, area.fromY - 1, area.fromX, tableMatrix);
-          }
-          if (area.toY === area.fromY) {
-            isNegative.current = true;
-          }
-          if (!isNegative.current) area.toY -= 1;
-          else {
-            if (area.fromY > 0) {
-              area.fromY -= 1;
-            }
-          }
-        }
+      area.toY = Math.max(0, area.toY - 1);
+      area.fromX = area.toX;
+      area.fromY = area.toY;
+    }
+    if (keyName === "down") {
+      // increace area.toY by 1 but not past the last row
+      area.toY = Math.min(tableMatrix.length - 1, area.toY + 1);
+      area.fromX = area.toX;
+      area.fromY = area.toY;
+    }
+    setSelectedAreas([{ ...area }]);
+  };
 
-        if (event.key === "ArrowLeft") {
-          if (area.toX === area.fromX) {
-            isNegative.current = true;
-          }
-          if (area.fromX > 0 && isNegative.current) area.fromX -= 1;
-          else {
-            if (area.toX > 0) area.toX -= 1;
-          }
-        }
+  const arrowShiftSelection = (keyName) => {
+    const area = selectedAreas[selectedAreas.length - 1];
+    const lastCol = tableMatrix[selectedAreas.length - 1].length - 1;
+    const lastRow = tableMatrix.length - 1;
 
-        if (event.key === "ArrowRight") {
-          if (area.toX === area.fromX) {
-            isNegative.current = false;
-          }
-          if (isNegative.current) { 
-            area.fromX += 1; 
-          }
-          else {
-            if (area.toX < lastCol) {
-              area.toX += 1;
-            }
-          }
+    if (keyName === "down") {
+      if (area.toY < lastRow) {
+        updateLabelArea(area, area.fromY + 1, area.fromX, tableMatrix);
+      }
+
+      if (area.toY === area.fromY) {
+        isNegative.current = false;
+      }
+      if (area.toY < lastRow) {
+        if (!isNegative.current) area.toY += 1;
+        else {
+          area.fromY += 1;
         }
-        setSelectedAreas([...selectedAreas]);
-      } else {
-        console.log('not shift key')
-        if (event.key === "ArrowRight") {
-          area.toX++;
-          area.fromX = area.toX;
-          area.fromY = area.toY;
-        }
-        if (event.key === "ArrowLeft") {
-          area.toX--;
-          area.fromX = area.toX;
-          area.fromY = area.toY;
-        }
-        if (event.key === "ArrowUp") {
-          area.toY--;
-          area.fromX = area.toX;
-          area.fromY = area.toY;
-        }
-        if (event.key === "ArrowDown") {
-          area.toY++;
-          area.fromX = area.toX;
-          area.fromY = area.toY;
-        }
-        setSelectedAreas([{ ...area }]);
       }
     }
 
-    window.addEventListener("keydown", handleSelectionKey);
+    if (keyName === "up") {
+      if (area.fromY > 0) {
+        updateLabelArea(area, area.fromY - 1, area.fromX, tableMatrix);
+      }
+      if (area.toY === area.fromY) {
+        isNegative.current = true;
+      }
+      if (!isNegative.current) area.toY -= 1;
+      else {
+        if (area.fromY > 0) {
+          area.fromY -= 1;
+        }
+      }
+    }
 
-    return () => {
-      window.removeEventListener("keydown", handleSelectionKey);
-    };
-  }, [selectedAreas, tableMatrix, isNegative]);
+    if (keyName === "left") {
+      if (area.toX === area.fromX) {
+        isNegative.current = true;
+      }
+      if (area.fromX > 0 && isNegative.current) area.fromX -= 1;
+      else {
+        if (area.toX > 0) area.toX -= 1;
+      }
+    }
+
+    if (keyName === "right") {
+      if (area.toX === area.fromX) {
+        isNegative.current = false;
+      }
+      if (isNegative.current) {
+        area.fromX += 1;
+      }
+      else {
+        if (area.toX < lastCol) {
+          area.toX += 1;
+        }
+      }
+    }
+    setSelectedAreas([...selectedAreas]);
+  }
+
 }
 
 function updateLabelArea(area, row, col, tableMatrix) {
