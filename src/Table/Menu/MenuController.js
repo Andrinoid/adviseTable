5c31f9a6b28eedc25e402667db2a020533c08792
Franklyn <franklynkleiton18@gmail.com>
2023-03-02 12:27:05 -0300

    Improve menu context limit area.

--- src/Table/Menu/MenuController.js
+++ src/Table/Menu/MenuController.js
@@ -1,25 +1,28 @@
-import Controller from './Controller';
+import Controller from "./Controller";
 
 export default class MenuController extends Controller {
-    getAdjustedX(clientX, menu, viewport) {
-      const menuRect = menu.getBoundingClientRect();
-      const viewportRect = viewport.getBoundingClientRect();
-      const rightBoundary = window.innerWidth - menuRect.width;
-    
-      return clientX > rightBoundary
-        ? clientX - menuRect.width - 245
-        : clientX - viewportRect.x;
-    }
-    
-    getAdjustedY(clientY, menu, viewport) {
-      const menuRect = menu.getBoundingClientRect();
-      const viewportRect = viewport.getBoundingClientRect();
-      const bottomBoundary = viewportRect.height + viewportRect.y - 150;
-    
-      return clientY > bottomBoundary
-        ? clientY - menuRect.height + 188
-        : clientY > window.innerHeight - 150
-        ? clientY - menuRect.height - 120
-        : clientY + 32 - viewportRect.y;
-    }
+  getAdjustedX(clientX, menu, viewport) {
+    const menuRect = menu.getBoundingClientRect();
+    const viewportRect = viewport.getBoundingClientRect();
+    const rightBoundary = window.innerWidth - menuRect.width;
+
+    return clientX > rightBoundary
+      ? clientX - menuRect.width - 245
+      : clientX - viewportRect.x;
   }
+
+  getAdjustedY(clientY, menu, viewport) {
+    const menuRect = menu.getBoundingClientRect();
+    const viewportRect = viewport.getBoundingClientRect();
+
+    const limit =
+      window.innerHeight -
+      window.innerHeight * (menuRect.height / window.innerHeight);
+    
+      if (clientY >= limit) {
+      return clientY - menuRect.height - 120;
+    }
+
+    return clientY + 32 - viewportRect.y;
+  }
+}
