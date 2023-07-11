d658346f5a98610cf4554b7bbf457ff57381fffd
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-07-11 08:22:34 -0300

    WIP: Create grid component

--- src/Grid/Item.js
+++ src/Grid/Item.js
@@ -3,16 +3,18 @@ import styled from "styled-components";
 import { Resizable } from "react-resizable";
 import "react-resizable/css/styles.css";
 
-
 const Container = styled.div`
   grid-column: ${(props) => props.column};
   grid-row: ${(props) => props.row};
   box-sizing: border-box;
-  height: ${(props) => typeof props.height == 'number' ? props.height + 'px' : props.height};
-  width: ${(props) => typeof props.width == 'number' ? props.width + 'px' : props.width};
 `;
 
-
+const ResizeableContainer = styled.div`
+  height: ${(props) =>
+    typeof props.height == "number" ? props.height + "px" : props.height};
+  width: ${(props) =>
+    typeof props.width == "number" ? props.width + "px" : props.width};
+`;
 
 const Item = forwardRef(({ children, ...rest }, ref) => {
   return (
@@ -22,48 +24,48 @@ const Item = forwardRef(({ children, ...rest }, ref) => {
   );
 });
 
-
 const ResizeableItem = ({ children, containerWidth, columns, ...rest }) => {
   const [size, setSize] = useState({ width: 0, height: 0 });
   const ref = useRef(null);
 
-
-
   useEffect(() => {
-    if (ref.current) {
+    const resize = () => {
       const e = ref.current;
       const r = e.getBoundingClientRect();
       setSize({ width: r.width, height: r.height });
+    };
+
+    if (ref.current) {
+      resize();
     }
+
+    window.addEventListener("resize", resize);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
   }, []);
 
-
-  const onResize = (event, { element, size: measures, handle }) => {
+  const onResize = (_, { size: measures }) => {
     setSize({ width: measures.width, height: size.height });
   };
 
-
-
   return (
-    <Item
-      ref={ref}
-      {...rest}
-      width={!!size.width ? size.width : "100%"}
-      height={!!size.height ? size.height : '100%'}
-    >
-      <Resizable
-        width={size.width}
-        height={size.height}
-        onResize={onResize}
-        resizeHandles={["se"]}
-        minConstraints={[200]}
-      >
-        {children}
-      </Resizable>
+    <Item ref={ref} {...rest}>
+      <ResizeableContainer height={size.height} width={size.width}>
+        <Resizable
+          width={size.width}
+          height={size.height}
+          onResize={onResize}
+          resizeHandles={["se"]}
+          minConstraints={[200]}
+        >
+          {children}
+        </Resizable>
+      </ResizeableContainer>
     </Item>
   );
 };
-
 
 export default ({ resizable, children, ...rest }) => {
   if (resizable) {
@@ -71,4 +73,4 @@ export default ({ resizable, children, ...rest }) => {
   }
 
   return <Item {...rest}>{children}</Item>;
-}
+};
