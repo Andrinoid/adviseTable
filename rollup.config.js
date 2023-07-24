523ad10bffccc4576a1f7af080bbd840519d8686
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-07-24 15:05:51 -0300

    Add postfix

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ const resolve = require("@rollup/plugin-node-resolve");
 const babel = require("@rollup/plugin-babel");
 const commonjs = require("@rollup/plugin-commonjs");
 const externalPeer = require("rollup-plugin-peer-deps-external");
+const postcss = require("rollup-plugin-postcss");
 
 module.exports = {
   input: "src/components/index.js",
@@ -20,5 +21,8 @@ module.exports = {
     resolve(),
     commonjs(),
     externalPeer(),
+    postcss({
+      extract: true,
+    }),
   ],
 };
