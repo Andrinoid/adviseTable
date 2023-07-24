81df94077bf3bf40bbfe662f205523c7686d0fd9
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-07-24 14:58:34 -0300

    Update exports.

--- src/components/Table/index.js
+++
@@ -1,7 +0,0 @@
-import Table from "./Table";
-import Header from "./Header";
-import Row from "./Row";
-import Col from "./Col";
-
-// expose the components
-export { Table, Header, Row, Col };
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
-export * from "./Grid";
+import Table from "./Table/Table";
+import Header from "./Table/Header";
+import Row from "./Table/Row";
+import Col from "./Table/Col";
+import Grid from "./Grid/Grid";
 
-export * from "./Table";
+// expose the components
+export { Table, Header, Row, Col, Grid };
