3bb5fbc189d00c0aa17695ea6c0a267db59f696e
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-08-07 15:10:39 -0300

    Add snap.

--- src/components/Grid/Resizer/index.js
+++ src/components/Grid/Resizer/index.js
@@ -7,7 +7,7 @@ import {
 } from "react";
 import styled from "styled-components";
 import { Dimensions, compute } from "../hooks";
-import { throttle } from "lodash";
+import { initial, throttle } from "lodash";
 
 export const Handler = styled.div`
   position: absolute;
@@ -34,50 +34,51 @@ export default function Resizer({
   const [x, setX] = useState(initialX);
   const ref = useRef(null);
   const resizing = useRef(false);
+  const snapped = useRef(false);
 
   useEffect(() => {
     setX(initialX);
   }, [initialX]);
 
+  const snap = useCallback(
+    (changedX) => {
+      const range = 15;
+      for (let ri = 0; ri < positionXs.length; ri++) {
+        if (ri !== rowIndex) {
+          for (let ci = 0; ci < positionXs[ri].length; ci++) {
+            const nextValue = positionXs[ri][ci];
+
+            if (changedX > nextValue - range && changedX < nextValue + range) {
+              return nextValue;
+            }
+          }
+        }
+      }
+
+      return changedX;
+    },
+    [positionXs]
+  );
+
   const handleOnMouseMove = useCallback(
     (e) => {
       e.preventDefault();
-      // function snap(changedX) {
-      //   const range = 50;
-      //   for (let ri = 0; ri < positionXs.length; ri++) {
-      //     if (ri !== rowIndex) {
-      //       for (let ci = 0; ci < positionXs[ri].length; ci++) {
-      //         const nextValue = positionXs[ri][ci];
-
-      //         if (
-      //           Math.round(x) == Math.round(nextValue) &&
-      //           changedX > nextValue - range &&
-      //           changedX < nextValue + range
-      //         ) {
-      //           console.log("entrou no false");
-      //           return [nextValue, false];
-      //         }
-      //       }
-      //     }
-      //   }
-
-      //   return [changedX, true];
-      // }
-      // TODO:
-      // -- snap to the next column
-      // -- stop resizer if min widths are reached
-      //    because right now the resizer is leaving the grid
 
       if (resizing.current) {
         const start = colIndex == 0 ? 0 : positionXs[rowIndex][colIndex - 1];
-        let width = e.clientX - leftGap - start;
+        const snappedX = snap(e.clientX - leftGap);
 
-        setX(e.clientX - leftGap);
+        let width = snappedX - start;
+
+        if (snappedX != e.clientX - leftGap) {
+          snapped.current = true;
+        }
 
         const newWidths = compute(
           new Dimensions(widths, colIndex, { width }, minWidth, totalWidth)
         );
         setWidths(newWidths, rowIndex);
+        setX(snappedX);
       }
     },
     [widths, colIndex, minWidth, totalWidth, leftGap, rowIndex, positionXs]
@@ -100,7 +101,12 @@ export default function Resizer({
     function handleOnMouseUp(e) {
       resizing.current = false;
       setResizing(false);
-      setX(initialX);
+
+      if (!snapped.current) {
+        setX(initialX);
+      } else {
+        snapped.current = false;
+      }
     }
 
     const throttled = throttle(handleOnClick, 1000);
@@ -124,7 +130,7 @@ export default function Resizer({
         window.removeEventListener("mouseup", handleOnMouseUp);
       }
     };
-  }, [x]);
+  }, [x, positionXs]);
 
   return <Handler ref={ref} x={x} />;
 }
