066903744317a05a7fb98f95dba73dd1f421c68a
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-08-09 19:58:35 -0300

    Export add section.

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import Header from "./Table/Header";
 import Row from "./Table/Row";
 import Col from "./Table/Col";
 import Grid from "./Grid/Grid";
+import AddSection from "./Grid/AddSection";
 
 // expose the components
-export { Table, Header, Row, Col, Grid };
+export { Table, Header, Row, Col, Grid, AddSection };
