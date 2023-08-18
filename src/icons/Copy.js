083660092d0cbd22e1ff7aad60499d31c1c3a1eb
Andri <andri@advise.is>
2023-08-18 15:42:27 +0000

    Update Copy.js

--- src/icons/Copy.js
+++ src/icons/Copy.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Copy(props) {
   return (
     <svg
-      enable-background="new 0 0 50 50"
+      enableBackground="new 0 0 50 50"
       height="0.9em"
       id="Layer_1"
       version="1.1"
@@ -13,29 +13,29 @@ export default function Copy(props) {
       <rect fill="none" height="50" width="50" />
       <polyline
         fill="none"
-        points="29,21 18,21 18,10   "
+        points="29,21 18,21 18,10"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-miterlimit="10"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
       />
       <polygon
         fill="none"
-        points="30,49 1,49 1,9   18,9 30,21 "
+        points="30,49 1,49 1,9 18,9 30,21"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-miterlimit="10"
-        stroke-width="3"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="3"
       />
       <polyline
         fill="none"
-        points="48,13 37,13 37,2   "
+        points="48,13 37,13 37,2"
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-miterlimit="10"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="2"
       />
-      <polygon points="37.414,0 19,0 19,6 21,8 21,2 36.586,2 48,13.414 48,40 33,40 33,42 50,42 50,12.586 " />
+      <polygon points="37.414,0 19,0 19,6 21,8 21,2 36.586,2 48,13.414 48,40 33,40 33,42 50,42 50,12.586" />
     </svg>
   );
 }
