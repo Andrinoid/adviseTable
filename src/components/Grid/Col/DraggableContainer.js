dd5d49140b4775447e050cbe626c924eb9e0536b
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-08-22 09:53:37 -0300

    refactor: create draggable container

---
+++ src/components/Grid/Col/DraggableContainer.js
@@ -0,0 +1,50 @@
+import React, { forwardRef } from "react";
+import { DraggableElm } from "./styled";
+import { Draggable } from "react-beautiful-dnd";
+import { DataContext } from "../Grid";
+import { useContext } from "react";
+import { getRowId } from "../hooks";
+
+export default function DraggableContainer({
+  draggableId,
+  index,
+  breakpoint,
+  children,
+}) {
+  const { colId, isResizing } = useContext(DataContext);
+  return (
+    <Draggable draggableId={draggableId} index={index}>
+      {(draggableProvided) => (
+        <DraggableElement
+          style={{ height: "100%" }}
+          ref={draggableProvided.innerRef}
+          {...draggableProvided.draggableProps}
+          $isDragging={draggableId == colId} //Only for styling. This is not part of dnd
+          breakpoint={breakpoint}
+          showHoverHandler={colId === null || colId === draggableId}
+          isResizing={colId === draggableId && isResizing}
+          styled={getRowId(colId) !== getRowId(draggableId)}
+        >
+          {children(draggableProvided.dragHandleProps, draggableId == colId)}
+        </DraggableElement>
+      )}
+    </Draggable>
+  );
+}
+
+const DraggableElement = forwardRef(
+  ({ children, styled = false, isResizing, ...rest }, ref) => {
+    if (styled) {
+      return (
+        <DraggableElm {...rest} isResizing={isResizing} ref={ref} style={{}}>
+          {children}
+        </DraggableElm>
+      );
+    }
+    return (
+      <DraggableElm {...rest} ref={ref} isResizing={isResizing}>
+        {children}
+      </DraggableElm>
+    );
+  }
+);
--- src/components/Grid/Col/index.js
+++ src/components/Grid/Col/index.js
@@ -1,10 +1,8 @@
-import React, { forwardRef, useEffect, useRef } from "react";
-import { Column, Inner, DraggableElm } from "./styled";
+import React, { useEffect, useRef } from "react";
+import { Column, Inner } from "./styled";
 import "react-resizable/css/styles.css";
-import { Draggable } from "react-beautiful-dnd";
 import { DataContext } from "../Grid";
 import { useContext } from "react";
-import { getRowId } from "../hooks";
 import Resizable from "../Resizable";
 import Toolbar from "./Toolbar";
 
@@ -21,10 +19,8 @@ function Col({
   onResize,
   resizeable,
 }) {
-  const { colId, isResizing, editing } = useContext(DataContext);
+  const { editing } = useContext(DataContext);
   const resizableRef = useRef(null);
-
-  const draggableId = rowId + "_" + columnId;
 
   useEffect(() => {
     if (resizableRef.current) {
@@ -33,69 +29,45 @@ function Col({
   }, [width]);
 
   return (
-    <Draggable draggableId={draggableId} index={index}>
-      {(draggableProvided) => (
-        <DraggableElement
-          style={{ height: "100%" }}
-          ref={draggableProvided.innerRef}
-          {...draggableProvided.draggableProps}
-          $isDragging={draggableId == colId} //Only for styling. This is not part of dnd
-          breakpoint={breakpoint}
-          showHoverHandler={colId === null || colId === draggableId}
-          isResizing={colId === draggableId && isResizing}
-          styled={getRowId(colId) !== getRowId(draggableId)}
+    <DraggableContainer
+      breakpoint={breakpoint}
+      draggableId={rowId + "_" + columnId}
+      index={index}
+    >
+      {(dragHandleProps, dragging) => (
+        <Resizable
+          ref={resizableRef}
+          key={index}
+          onResizeStart={onResizeStart}
+          onResizeEnd={onResizeEnd}
+          onResize={onResize}
+          enabled={resizeable}
         >
-          <Resizable
-            ref={resizableRef}
-            key={index}
-            onResizeStart={onResizeStart}
-            onResizeEnd={onResizeEnd}
-            onResize={onResize}
-            enabled={resizeable}
+          <Column
+            id={"col_" + columnId}
+            $isDragging={dragging}
+            style={{
+              width: (width || 0) * totalWidth,
+              flex: (width || 0) * totalWidth,
+              height: "100%",
+            }}
+            breakpoint={breakpoint}
+            editing={editing}
           >
-            <Column
-              id={"col_" + columnId}
-              $isDragging={draggableId == colId}
-              style={{
-                width: (width || 0) * totalWidth,
-                flex: (width || 0) * totalWidth,
-                height: "100%",
-              }}
-              breakpoint={breakpoint}
-              editing={editing}
-            >
-              {editing && (
-                <Toolbar
-                  dragHandleProps={draggableProvided.dragHandleProps}
-                  rowId={rowId}
-                  columnId={columnId}
-                />
-              )}
+            {editing && (
+              <Toolbar
+                dragHandleProps={dragHandleProps}
+                rowId={rowId}
+                columnId={columnId}
+              />
+            )}
 
-              <Inner>{children}</Inner>
-            </Column>
-          </Resizable>
-        </DraggableElement>
+            <Inner>{children}</Inner>
+          </Column>
+        </Resizable>
       )}
-    </Draggable>
+    </DraggableContainer>
   );
 }
-
-const DraggableElement = forwardRef(
-  ({ children, styled = false, isResizing, ...rest }, ref) => {
-    if (styled) {
-      return (
-        <DraggableElm {...rest} isResizing={isResizing} ref={ref} style={{}}>
-          {children}
-        </DraggableElm>
-      );
-    }
-    return (
-      <DraggableElm {...rest} ref={ref} isResizing={isResizing}>
-        {children}
-      </DraggableElm>
-    );
-  }
-);
 
 export default Col;
