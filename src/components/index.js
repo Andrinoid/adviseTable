b052953c0db79806e5b48e603945cb5b8e99bdc1
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-09-07 14:47:52 -0300

    Remove contextmenu

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,5 @@ import Col from "./Table/Col";
 import Grid from "./Grid/Grid";
 import AddSection from "./Grid/AddSection";
 
-export * as ContextMenu from "./ContextMenu";
-
 // expose the components
 export { Table, Header, Row, Col, Grid, AddSection };
