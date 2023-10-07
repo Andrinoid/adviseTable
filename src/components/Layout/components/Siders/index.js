1b8dd3e13c09e80bfcf1ad518fdcc3a86d7185b4
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-10-07 15:24:02 -0300

    Add animation.

--- src/components/Layout/components/Siders/index.js
+++ src/components/Layout/components/Siders/index.js
@@ -1,16 +1,31 @@
 import React from "react";
 import useLayout from "../../hooks/useLayout";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Siders = ({ children }) => {
   const { siders } = useLayout();
 
+  const transition = {
+    stiffness: 10,
+    duration: 0.025,
+  };
+
   return (
     <>
       {children}
-
-      {siders.map((sider, siderIndex) => {
-        return <>{sider[sider.length - 1](siderIndex)}</>;
-      })}
+      <AnimatePresence>
+        {siders.map((sider, siderIndex) => (
+          <motion.div
+            key={siderIndex}
+            transition={transition}
+            initial={{ x: -50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -50, opacity: 0 }}
+          >
+            {sider[sider.length - 1](siderIndex)}
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </>
   );
 };
