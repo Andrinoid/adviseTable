6b491100bc302086339aef450a25b6060610ec76
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-10-08 12:21:45 -0300

    Move reseable to class and change animation.

---
+++ src/components/Layout/components/Sider/Resizeable/styles.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  height: 100%;
+  overflow-x: hidden;
+  box-sizing: border-box;
+  flex-grow: 0;
+  user-select: none;
+  transition: ${({ isResizing }) =>
+    isResizing ? "none" : "width 0.2s ease, opacity 0.1s ease"};
+  flex-shrink: 0;
+  width: 100%;
+`;
+
+export const Handle = styled.div`
+  position: absolute;
+  top: 0;
+  ${({ right }) => {
+    if (right) {
+      return "left: 0;";
+    } else {
+      return "right: 0;";
+    }
+  }}
+  width: 4px;
+  height: 100%;
+  cursor: col-resize;
+  background-color: ${({ hoverActive, isResizing }) =>
+    hoverActive || isResizing ? "rgb(232 232 232 / 79%)" : "transparent"};
+  transition: background-color 0.3s ease;
+  &:hover {
+    background-color: rgb(232 232 232 / 79%);
+    transition: background-color 0.5s ease 0.5s;
+  }
+`;
