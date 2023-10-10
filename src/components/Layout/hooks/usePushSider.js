f66a25195dd37132c267ceac1b943e4ad85b5069
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-10-10 11:04:11 -0300

    clean up

--- src/components/Layout/hooks/usePushSider.js
+++ src/components/Layout/hooks/usePushSider.js
@@ -1,27 +1,26 @@
 import React from "react";
 import { produce } from "immer";
-
 import useLayout from "./useLayout";
 
 const usePushSider = () => {
   const { siders, setSiders, setDrawers } = useLayout();
 
   return (value) => {
-    setSiders(
-      produce(siders, (draft) => {
-        const exists = siders.some(
-          (sider) => sider[0] && sider[0](0).type === value(0).type
-        );
-
-        if (!exists) {
-          draft = [...draft, [value]];
-
-          setDrawers([]);
-        }
-
-        return draft;
-      })
+    const exists = siders.some(
+      (sider) =>
+        sider[0] &&
+        sider[0](0).type === value(0).type &&
+        JSON.stringify(sider[0](0).props) === JSON.stringify(value(0).props)
     );
+
+    if (!exists) {
+      const updatedSiders = produce(siders, (draft) => {
+        draft.push([value]);
+      });
+
+      setSiders(updatedSiders);
+      setDrawers([]);
+    }
   };
 };
 
