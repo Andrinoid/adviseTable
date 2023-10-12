d775b0414d3ae81842ba91dcae1fb8bf45637753
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-10-12 09:37:43 -0300

    Fix

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import Header from "./Table/Header";
 import Row from "./Table/Row";
 import Col from "./Table/Col";
 import Grid from "./Grid/Grid";
-import Layout from "./Layout";
 import AddSection from "./Grid/AddSection";
 
 import Layout from "./Layout/components/Layout";
