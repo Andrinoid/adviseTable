72c36ad2a96b1200ff657fd99a1a57246aff1ac0
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-10-17 11:47:33 -0300

    make background disappear

--- src/components/Layout/components/Drawer/styles.js
+++ src/components/Layout/components/Drawer/styles.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: rgb(248, 250, 251);
+  background-color: transparent;
   height: 100%;
   width: 100%;
   position: absolute;
   top: 0;
-  border-right: 1px solid rgb(232, 234, 237);
 
   @media (max-width: 700px) {
     width: calc(100vw - 60px);
