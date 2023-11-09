d0036aa4a71aaeadd9b049e12bf80ae52ffe6f3a
Andri <andri@advise.is>
2023-11-09 11:16:28 +0000

    styles

--- src/components/Layout/components/Content/index.js
+++ src/components/Layout/components/Content/index.js
@@ -12,24 +12,18 @@ const Content = ({ children }) => {
     }, [isContentScrolled]);
 
     useEffect(() => {
-        // This function checks the scroll position and updates the context accordingly
         const updateScrollY = () => {
-            // Check if the ref is currently being scrolled
             const isScrolled = ref.current.scrollTop > 0;
             setIsContentScrolled(isScrolled);
         };
 
-        // Set up the event listener to call updateScrollY when the container is scrolled
         const handleScroll = () => {
             updateScrollY();
         };
 
-        // Assuming the ref is attached to a scrollable element,
-        // we need to add the event listener to the ref's current element
         const container = ref.current;
         container.addEventListener("scroll", handleScroll);
 
-        // Remove the event listener on cleanup
         return () => container.removeEventListener("scroll", handleScroll);
     }, [setIsContentScrolled]);
 
--- src/components/Layout/components/Sider/styles.js
+++ src/components/Layout/components/Sider/styles.js
@@ -1,27 +1,26 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  width: ${({ width }) => {
-    if (width && typeof width == "number") {
-      return `${width}px`;
-    }
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: ${({ width }) => {
+        if (width && typeof width == "number") {
+            return `${width}px`;
+        }
 
-    if (width && typeof width == "string" && width.includes("%")) {
-      return width;
-    }
+        if (width && typeof width == "string" && width.includes("%")) {
+            return width;
+        }
 
-    return "60px";
-  }};
-  background-color: #242a43;
-  border-right: 1px solid #e8eaed;
-  border-left: 1px solid #e8eaed;
-  background: #f8fafb;
-  flex-shrink: 0;
-  z-index: 2;
-  border-left-width: ${({ borderLeftWidth }) => borderLeftWidth}px;
-  border-right-width: ${({ borderRightWidth }) => borderRightWidth}px;
-  pointer-events: initial;
+        return "60px";
+    }};
+    border-right: 1px solid #e8eaed;
+    border-left: 1px solid #e8eaed;
+    background: #ffffff;
+    flex-shrink: 0;
+    z-index: 2;
+    border-left-width: ${({ borderLeftWidth }) => borderLeftWidth}px;
+    border-right-width: ${({ borderRightWidth }) => borderRightWidth}px;
+    pointer-events: initial;
 `;
