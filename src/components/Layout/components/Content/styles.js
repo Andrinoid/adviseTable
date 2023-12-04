0c5bd4c56f6449c0c574585592ad089517ddb9d1
Andri <andri@advise.is>
2023-12-04 14:03:35 +0000

    Update styles.js

--- src/components/Layout/components/Content/styles.js
+++ src/components/Layout/components/Content/styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  overflow: auto;
-  height: 100%;
-  width: ${({ width }) => (width ? `${width}px` : "100%")};
+    overflow: hidden;
+    height: 100%;
+    width: ${({ width }) => (width ? `${width}px` : "100%")};
 
-  @media (min-width: 700px) {
-    position: relative;
-  }
+    @media (min-width: 700px) {
+        position: relative;
+    }
 `;
