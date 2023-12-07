9fa59d96342f7aefe6dcc1fa6640f03e368413d8
Franklyn Kleiton <franklynkleiton18@gmail.com>
2023-12-07 16:53:53 -0300

    Fix: When a drawer is opened we show the active siderItem, but when the drawer is closed we need to remove the active siderItem. Now it remains

--- src/components/Layout/components/SiderItems/index.js
+++ src/components/Layout/components/SiderItems/index.js
@@ -2,37 +2,40 @@ import React, { useEffect, useState } from "react";
 import useControls from "../../hooks";
 
 const SiderItems = ({ children, besides, ...rest }) => {
-    const { drawers, siders, links } = useControls();
-    const [active, setActive] = useState(null);
+  const { drawers, siders, links } = useControls();
+  const [active, setActive] = useState(null);
 
-    useEffect(() => {
-        if (drawers.length === 0 && siders.length === 0 && links.length === 0) {
-            setActive(null);
+  useEffect(() => {
+    if (
+      (drawers.length === 0 && links.length === 0) ||
+      (siders.length === 0 && links.length === 0)
+    ) {
+      setActive(null);
+    }
+  }, [siders, drawers, links]);
+
+  return (
+    <div {...rest} style={{ marginTop: 8 }}>
+      {React.Children.map(children, (child) => {
+        if (child && child.props && child.props.id) {
+          const obj = {
+            onClick: () => {
+              setActive(child.props.id);
+
+              if (child.props.onClick) {
+                child.props.onClick();
+              }
+            },
+            active: child.props.id && child.props.id === active,
+          };
+
+          return React.cloneElement(child, obj);
         }
-    }, [siders, drawers, links]);
 
-    return (
-        <div {...rest} style={{ marginTop: 8 }}>
-            {React.Children.map(children, (child) => {
-                if (child && child.props && child.props.id) {
-                    const obj = {
-                        onClick: () => {
-                            setActive(child.props.id);
-
-                            if (child.props.onClick) {
-                                child.props.onClick();
-                            }
-                        },
-                        active: child.props.id && child.props.id === active,
-                    };
-
-                    return React.cloneElement(child, obj);
-                }
-
-                return child;
-            })}
-        </div>
-    );
+        return child;
+      })}
+    </div>
+  );
 };
 
 export default SiderItems;
