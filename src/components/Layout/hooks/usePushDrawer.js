e483985ade11779ef1c82ea2c1ac41fe15fde583
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-01-16 10:55:33 -0300

    Improve push drawer.

--- src/components/Layout/hooks/usePushDrawer.js
+++ src/components/Layout/hooks/usePushDrawer.js
@@ -2,18 +2,33 @@ import React from "react";
 import produce from "immer";
 
 import useLayout from "./useLayout";
+import useClear from "./useClear";
 
 const usePushDrawer = () => {
-  const { drawers, setDrawers } = useLayout();
+  const { drawers, setDrawers, siders } = useLayout();
+  const clear = useClear();
 
   return (component) => {
-    setDrawers(
-      produce(drawers, (draft) => {
-        draft = [component];
+    if (siders.length > 0) {
+      clear();
+      setTimeout(() => {
+        setDrawers(
+          produce(drawers, (draft) => {
+            draft = [component];
 
-        return draft;
-      })
-    );
+            return draft;
+          })
+        );
+      }, 200);
+    } else {
+      setDrawers(
+        produce(drawers, (draft) => {
+          draft = [component];
+
+          return draft;
+        })
+      );
+    }
   };
 };
 
