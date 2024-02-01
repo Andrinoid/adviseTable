4a298c12e252b652108372ca392e2d7b91087e56
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-02-01 10:08:59 -0300

    Fix autosize issue

--- src/components/shared/useAutoResize.js
+++ src/components/shared/useAutoResize.js
@@ -1,19 +1,32 @@
-import React, { useEffect } from "react";
+import { debounce } from "lodash";
+import React, { useEffect, useRef } from "react";
 
 export default function useAutoResize(ref, resizeCallback) {
-  let containerWidth = ref && ref.current ? ref.current.offsetWidth : 0;
+  const containerWidth = useRef(0);
+
   useEffect(() => {
     if (ref && ref.current) {
-      const resizeObserver = new ResizeObserver(() => {
-        if (containerWidth !== ref.current.offsetWidth) {
-          resizeCallback(ref.current.offsetWidth);
-          containerWidth = ref.current.offsetWidth;
-        }
-      });
+      const resizeObserver = new ResizeObserver(
+        debounce((entries) => {
+          for (const entry of entries) {
+            const width = entry.borderBoxSize?.[0].inlineSize;
+            if (
+              typeof width === "number" &&
+              Math.abs(width - containerWidth.current) > 10
+            ) {
+              containerWidth.current = width;
+              console.log(width, "width");
+              resizeCallback(ref.current.offsetWidth);
+            }
+          }
+        }, 500)
+      );
+
       resizeObserver.observe(ref.current);
+
       return () => {
         resizeObserver.disconnect();
       };
     }
-  }, []);
+  }, [ref, resizeCallback]);
 }
