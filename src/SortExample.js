a0cf6dfa3405186e3a48068759cbee185c15403c
Andri <andri@advise.is>
2024-02-02 17:07:20 +0000

    Update SortExample.js

--- src/SortExample.js
+++ src/SortExample.js
@@ -1,5 +1,17 @@
 import React, { useState } from 'react';
 import SortableView from './components/Sortable';
+import styled from 'styled-components';
+
+const PageWrapper = styled.div`
+  padding: 20px;
+`;
+
+const Item = styled.div`
+  padding: 10px;
+  box-shadow: #000000 0px 0px 0px 1px;
+  cursor: pointer;
+  background-color: #fff;
+`;
 
 function SortExample() {
   const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4']);
@@ -11,13 +23,13 @@ function SortExample() {
   };
   return (
     // Your JSX code goes here
-    <div>
+    <PageWrapper>
       <SortableView draggable={true} onOrderChange={handleOnOrderChange}>
         {list.map((item, index) => (
-          <div key={index}>{item}</div>
+          <Item key={index}>{item}</Item>
         ))}
       </SortableView>
-    </div>
+    </PageWrapper>
   );
 }
 
