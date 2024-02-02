8d68984e1bfafddc66d2c1adf82f8005e6fdbcb6
Gustavo Sizílio <gustavosizilio@gmail.com>
2024-02-02 15:34:11 -0300

    adding the export

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Row from './Table/Row';
 import Col from './Table/Col';
 import Grid from './Grid/Grid';
 import AddSection from './Grid/AddSection';
-import SortableView from './SortableView/SortableView';
+import SortableView from './Sortable/SortableView';
 
 import Layout from './Layout/components/Layout';
 import Sider from './Layout/components/Sider';
