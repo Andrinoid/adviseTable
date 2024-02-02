89641b907e6dce08fe52bf2f5f236e6619966d53
Gustavo Sizílio <gustavosizilio@gmail.com>
2024-02-02 15:06:16 -0300

    fix on end capute to remove indicator

--- src/components/Sortable/index.js
+++ src/components/Sortable/index.js
@@ -9,35 +9,45 @@ function SortableView({ draggable, children, onOrderChange }) {
     setFromIndex(key);
   }, []);
 
-  const onDragOver = useCallback((e) => {
-    cancelDefault(e);
-    const overIndex = parseInt(e.currentTarget.dataset.draggablekey, 10);
-    if (overIndex !== hoverIndex) {
-      setHoverIndex(overIndex);
-    }
-  }, [hoverIndex]);
-
-  const onDrop = useCallback((e) => {
-    cancelDefault(e)
-    const toIndex = parseInt(e.currentTarget.dataset.draggablekey, 10);
-    // if (fromIndex === toIndex) return;
-
-    if (typeof onOrderChange === 'function') {
-      onOrderChange(fromIndex, toIndex);
-    }
-    //Reset states after drop
-    setFromIndex(null); 
+  const onDragEndCapture = useCallback(() => {
     setHoverIndex(null);
-  }, [fromIndex, onOrderChange]);
+  });
+
+  const onDragOver = useCallback(
+    (e) => {
+      cancelDefault(e);
+      const overIndex = parseInt(e.currentTarget.dataset.draggablekey, 10);
+      if (overIndex !== hoverIndex) {
+        setHoverIndex(overIndex);
+      }
+    },
+    [hoverIndex],
+  );
+
+  const onDrop = useCallback(
+    (e) => {
+      cancelDefault(e);
+      const toIndex = parseInt(e.currentTarget.dataset.draggablekey, 10);
+      // if (fromIndex === toIndex) return;
+
+      if (typeof onOrderChange === 'function') {
+        onOrderChange(fromIndex, toIndex);
+      }
+      //Reset states after drop
+      setFromIndex(null);
+      setHoverIndex(null);
+    },
+    [fromIndex, onOrderChange],
+  );
 
   const cancelDefault = useCallback((e) => {
     e.preventDefault();
     e.stopPropagation();
   }, []);
 
-  const indicatorStyle = { 
-    height: '2px', 
-    backgroundColor: 'blue', 
+  const indicatorStyle = {
+    height: '2px',
+    backgroundColor: 'blue',
     margin: '4px 0',
   };
 
@@ -45,20 +55,23 @@ function SortableView({ draggable, children, onOrderChange }) {
     <div>
       {React.Children.map(children, (child, index) => (
         <>
-        
-        <div
-          draggable={draggable}
-          onDragStart={onDragStart}
-          onDrop={onDrop}
-          onDragOver={onDragOver}
-          key={index}
-          data-draggablekey={index}
-        >
-          {child}
-        </div>
-        {hoverIndex === index && (
-          <div style={indicatorStyle} data-draggablekey={`indicator-${index}`}></div>
-        )}
+          <div
+            draggable={draggable}
+            onDragStart={onDragStart}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            onDragEndCapture={onDragEndCapture}
+            key={index}
+            data-draggablekey={index}
+          >
+            {child}
+          </div>
+          {hoverIndex === index && (
+            <div
+              style={indicatorStyle}
+              data-draggablekey={`indicator-${index}`}
+            ></div>
+          )}
         </>
       ))}
     </div>
