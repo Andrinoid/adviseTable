bb80c662a517dbfa8491bb84d699690ffb3ae345
Gustavo Sizílio <gustavosizilio@gmail.com>
2024-02-02 15:30:29 -0300

    adding the export

--- src/components/index.js
+++ src/components/index.js
@@ -1,41 +1,43 @@
-import Table from "./Table/Table";
-import Header from "./Table/Header";
-import Row from "./Table/Row";
-import Col from "./Table/Col";
-import Grid from "./Grid/Grid";
-import AddSection from "./Grid/AddSection";
+import Table from './Table/Table';
+import Header from './Table/Header';
+import Row from './Table/Row';
+import Col from './Table/Col';
+import Grid from './Grid/Grid';
+import AddSection from './Grid/AddSection';
+import SortableView from './SortableView/SortableView';
 
-import Layout from "./Layout/components/Layout";
-import Sider from "./Layout/components/Sider";
-import LayoutHeader from "./Layout/components/Header";
-import Content from "./Layout/components/Content";
-import Footer from "./Layout/components/Footer";
-import Drawer from "./Layout/components/Drawer";
-import Siders from "./Layout/components/Siders";
-import SiderItems from "./Layout/components/SiderItems";
-import SiderItem from "./Layout/components/SiderItem";
-import Logo from "./Layout/components/Logo";
-import useControls from "./Layout/hooks";
-import { LayoutProvider } from "./Layout/LayoutContext";
+import Layout from './Layout/components/Layout';
+import Sider from './Layout/components/Sider';
+import LayoutHeader from './Layout/components/Header';
+import Content from './Layout/components/Content';
+import Footer from './Layout/components/Footer';
+import Drawer from './Layout/components/Drawer';
+import Siders from './Layout/components/Siders';
+import SiderItems from './Layout/components/SiderItems';
+import SiderItem from './Layout/components/SiderItem';
+import Logo from './Layout/components/Logo';
+import useControls from './Layout/hooks';
+import { LayoutProvider } from './Layout/LayoutContext';
 
 // expose the components
 export {
-    Table,
-    Header,
-    Row,
-    Col,
-    Grid,
-    AddSection,
-    Layout,
-    Sider,
-    LayoutHeader,
-    Content,
-    Footer,
-    Drawer,
-    Siders,
-    useControls,
-    LayoutProvider,
-    SiderItems,
-    SiderItem,
-    Logo,
+  Table,
+  Header,
+  Row,
+  Col,
+  Grid,
+  AddSection,
+  Layout,
+  Sider,
+  LayoutHeader,
+  Content,
+  Footer,
+  Drawer,
+  Siders,
+  useControls,
+  LayoutProvider,
+  SiderItems,
+  SiderItem,
+  Logo,
+  SortableView,
 };
