24aaf336e2fc90109e3dd5ef17f4417ec7a7e556
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-02-05 11:36:04 -0300

    Check maxWidth type

--- src/components/Layout/components/Sider/Resizeable/styles.js
+++ src/components/Layout/components/Sider/Resizeable/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -8,18 +8,18 @@ export const Container = styled.div`
   flex-grow: 0;
   user-select: none;
   transition: ${({ isResizing }) =>
-    isResizing ? "none" : "width 0.2s ease, opacity 0.1s ease"};
+    isResizing ? 'none' : 'width 0.2s ease, opacity 0.1s ease'};
   flex-shrink: 0;
   width: ${({ width }) => width}px;
 
   ${({ maxWidth }) => {
-    if (maxWidth) {
+    if (maxWidth && maxWidth !== Infinity) {
       return `
         max-width: ${maxWidth}px;
       `;
     }
 
-    return "";
+    return '';
   }}
 `;
 
@@ -31,7 +31,7 @@ export const Handle = styled.div`
   height: 100%;
   cursor: col-resize;
   background-color: ${({ hoverActive, isResizing }) =>
-    hoverActive || isResizing ? "rgb(232 232 232 / 79%)" : "transparent"};
+    hoverActive || isResizing ? 'rgb(232 232 232 / 79%)' : 'transparent'};
   transition: background-color 0.3s ease;
   &:hover {
     background-color: rgb(232 232 232 / 79%);
