324539e03f8cfe4e731f3de40f044cde0dcf8437
Andri <andri@advise.is>
2024-02-07 15:42:05 +0000

    fix issue on the layout when overflow Y is removed

--- src/components/Layout/components/Layout/styles.js
+++ src/components/Layout/components/Layout/styles.js
@@ -5,6 +5,8 @@ export const Container = styled.div`
   display: flex;
   height: 100%;
   width: 100%;
+  overflow-y: hidden;
+  //Maybe we should have overflow-x auto and remove the overflow-y hidden
   position: relative;
   ${({ vertical }) => {
     if (vertical) {
