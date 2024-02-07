aab9d3ec39b6932824855f314782c12436a83815
Andri <andri@advise.is>
2024-02-07 14:22:14 +0000

    fix undefined css issue

--- src/components/Layout/components/Sider/styles.js
+++ src/components/Layout/components/Sider/styles.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
     border-right: 1px solid #e8eaed;
     background: #ffffff;
     flex-shrink: 0;
-    border-left-width: ${({ borderLeftWidth }) => borderLeftWidth}px;
-    border-right-width: ${({ borderRightWidth }) => borderRightWidth}px;
+    border-left-width: ${({ borderLeftWidth }) => borderLeftWidth || 0}px;
+    border-right-width: ${({ borderRightWidth }) => borderRightWidth || 0}px;
     pointer-events: initial;
 `;
