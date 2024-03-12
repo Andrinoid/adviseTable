e5a394229f229cb64350bf2c2340df7ca694e751
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-03-12 10:21:01 -0300

    Use elements instead reactjs components

--- src/components/Layout/components/Siders/index.js
+++ src/components/Layout/components/Siders/index.js
@@ -1,38 +1,27 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { register } from 'swiper/element/bundle';
 import useLayout from '../../hooks/useLayout';
 import 'swiper/css';
+
+register();
 
 const Siders = ({ children }) => {
   let { siders, mobile } = useLayout();
 
-  const SwiperDecorator = mobile ? Swiper : React.Fragment;
+  const Swiper = mobile
+    ? ({ children }) => <swiper-container>{children}</swiper-container>
+    : React.Fragment;
 
-  const SwiperSlideDecorator = mobile ? SwiperSlide : React.Fragment;
-
-  const Container =
-    mobile && siders.length >= 1
-      ? ({ children }) => (
-          <div
-            style={{
-              height: '100%',
-              width: '100%',
-              position: 'relative',
-              display: 'flex',
-            }}
-          >
-            {children}
-          </div>
-        )
-      : React.Fragment;
+  const SwiperSlide = mobile
+    ? ({ children }) => <swiper-slide>{children}</swiper-slide>
+    : React.Fragment;
 
   return (
     <>
       {children}
 
-      <Container>
-        <SwiperDecorator>
+      <div style={mobile ? { height: '100%', width: '100%' } : {}}>
+        <Swiper>
           {siders.map((sider, siderIndex) => {
             let current = null;
 
@@ -43,7 +32,7 @@ const Siders = ({ children }) => {
             }
 
             return (
-              <SwiperSlideDecorator>
+              <SwiperSlide>
                 <div
                   style={{
                     height: '100vh',
@@ -52,11 +41,11 @@ const Siders = ({ children }) => {
                 >
                   {current}
                 </div>
-              </SwiperSlideDecorator>
+              </SwiperSlide>
             );
           })}
-        </SwiperDecorator>
-      </Container>
+        </Swiper>
+      </div>
     </>
   );
 };
