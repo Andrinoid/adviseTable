8957b3c2c89ea385e93a92d14c39db77649e2467
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-03-12 10:45:12 -0300

    change containers

--- src/components/Layout/components/Siders/index.js
+++ src/components/Layout/components/Siders/index.js
@@ -8,20 +8,17 @@ register();
 const Siders = ({ children }) => {
   let { siders, mobile } = useLayout();
 
-  const Swiper = mobile
-    ? ({ children }) => <swiper-container>{children}</swiper-container>
-    : React.Fragment;
-
-  const SwiperSlide = mobile
-    ? ({ children }) => <swiper-slide>{children}</swiper-slide>
-    : React.Fragment;
-
   return (
     <>
       {children}
 
-      <div style={mobile ? { height: '100%', width: '100%' } : {}}>
-        <Swiper>
+      <div
+        style={{
+          height: mobile ? '100%' : 'initial',
+          width: mobile ? '100%' : 'initial',
+        }}
+      >
+        <swiper-container>
           {siders.map((sider, siderIndex) => {
             let current = null;
 
@@ -31,20 +28,9 @@ const Siders = ({ children }) => {
               current = sider;
             }
 
-            return (
-              <SwiperSlide>
-                <div
-                  style={{
-                    height: '100vh',
-                    zIndex: 9,
-                  }}
-                >
-                  {current}
-                </div>
-              </SwiperSlide>
-            );
+            return <swiper-slide>{current}</swiper-slide>;
           })}
-        </Swiper>
+        </swiper-container>
       </div>
     </>
   );
