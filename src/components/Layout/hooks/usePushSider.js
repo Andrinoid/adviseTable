df2e4c5c54afd9f8e7030c339476f5a189191756
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-04-09 16:51:59 -0300

    fix pushsider state issue

--- src/components/Layout/hooks/usePushSider.js
+++ src/components/Layout/hooks/usePushSider.js
@@ -1,31 +1,34 @@
-import React from "react";
-import { produce } from "immer";
-import useLayout from "./useLayout";
+import React from 'react';
+import { produce } from 'immer';
+import useLayout from './useLayout';
 
 const usePushSider = () => {
   const { siders, setSiders, setDrawers, drawers } = useLayout();
 
   const pushSider = (value, reset = false) => {
-    const updatedSiders = produce(siders, (draft) => {
-      if (reset) {
-        return [[value]];
-      }
+    setSiders((previous) => {
+      const updatedSiders = produce(previous, (draft) => {
+        if (reset) {
+          return [[value]];
+        }
 
-      const exists = siders.some(
-        (sider) =>
-          sider[0] &&
-          sider[0](0).type === value(0).type &&
-          sider[0](0).props.name === value(0).props.name
-      );
+        const exists = siders.some(
+          (sider) =>
+            sider[0] &&
+            sider[0](0).type === value(0).type &&
+            sider[0](0).props.name === value(0).props.name,
+        );
 
-      if (!exists) {
-        draft.push([value]);
-      }
+        if (!exists) {
+          draft.push([value]);
+        }
 
-      return draft;
+        return draft;
+      });
+
+      return updatedSiders;
     });
 
-    setSiders(updatedSiders);
     if (drawers.length > 0) {
       setDrawers([]);
     }
