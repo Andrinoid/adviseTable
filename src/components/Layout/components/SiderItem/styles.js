cdde5920ca7f05bcceaa4887efbcc015ec808d11
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-04-10 15:00:48 -0300

    change to color without opacity

--- src/components/Layout/components/SiderItem/styles.js
+++ src/components/Layout/components/SiderItem/styles.js
@@ -1,68 +1,65 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    border: none;
-    box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    font-size: 15px;
-    cursor: pointer;
-    position: relative;
-    padding: 2px 4px;
-    img {
-        width: 100%;
-        height: auto;
-        max-width: 60px;
-    }
+  border: none;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  cursor: pointer;
+  position: relative;
+  padding: 2px 4px;
+  img {
+    width: 100%;
+    height: auto;
+    max-width: 60px;
+  }
 `;
 
 export const SmallTile = styled.div`
-    background: ${(props) =>
-        props.outstanding
-            ? "linear-gradient(133deg, rgb(91 247 167 / 80%), rgb(43 94 235 / 80%));"
-            : props.active
-            ? "#228be61a"
-            : "transparent"};
-    border-radius: 5px;
-    width: 45px;
-    height: 45px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding: 0 10px;
-    svg {
-        stroke-width: 1.4;
-        stroke: ${(props) =>
-            props.outstanding ? "#fff" : props.active ? "#1677ff" : "#4a5058"};
-    }
+  background: ${(props) =>
+    props.outstanding
+      ? 'linear-gradient(133deg, rgb(91 247 167 / 80%), rgb(43 94 235 / 80%));'
+      : props.active
+      ? '#E7F2FC'
+      : 'transparent'};
+  border-radius: 5px;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0 10px;
+  svg {
+    stroke-width: 1.4;
+    stroke: ${(props) =>
+      props.outstanding ? '#fff' : props.active ? '#1677ff' : '#4a5058'};
+  }
 
-    &:hover {
-        background-color: ${(props) =>
-            props.active ? "#228be61a" : "#f8f9fa"};
-    }
+  &:hover {
+    background-color: ${(props) => (props.active ? '#228be61a' : '#f8f9fa')};
+  }
 `;
 
 export const Tile = styled.div`
-    background-color: ${(props) =>
-        props.active ? "#228be61a" : "transparent"};
-    border-radius: 5px;
-    width: 100%;
-    height: 45px;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding: 0 10px;
-    svg {
-        stroke-width: 1.4;
-        stroke: ${(props) => (props.active ? "#1677ff" : "#4a5058")};
-    }
-    color: ${(props) => (props.active ? "#1677ff" : "inherit")};
-    &:hover {
-        background-color: ${(props) =>
-            props.active ? "#228be61a" : "#f8f9fa"};
-    }
+  background-color: ${(props) => (props.active ? '#228be61a' : 'transparent')};
+  border-radius: 5px;
+  width: 100%;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0 10px;
+  svg {
+    stroke-width: 1.4;
+    stroke: ${(props) => (props.active ? '#1677ff' : '#4a5058')};
+  }
+  color: ${(props) => (props.active ? '#1677ff' : 'inherit')};
+  &:hover {
+    background-color: ${(props) => (props.active ? '#E7F2FC' : '#f8f9fa')};
+  }
 `;
 
 export const Icon = styled.span`
-    line-height: 0;
+  line-height: 0;
 `;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import ExampleSimple from './ExampleSimple';
 import { RootStyle } from './global.style';
 import SortExample from './SortExample';
-import App from './components/Grid/App';
+// import App from './components/Grid/App';
 // import App from "./components/ContextMenu/App";
-// import App from './components/Layout/App';
+import App from './components/Layout/App';
 
 const Application = () => (
   <>
