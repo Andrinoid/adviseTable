83b5f725a651a03605ffd94fa33b83716c2778fd
Andri <andri@advise.is>
2024-06-05 15:14:55 +0000

    Update index.js

--- src/components/Layout/components/Sider/index.js
+++ src/components/Layout/components/Sider/index.js
@@ -34,10 +34,12 @@ const Sider = ({
   }
 
   if (platform === 'mobile' && !main) {
+    console.log('mobileContainer rendered');
     return <MobileContainer {...rest}>{children}</MobileContainer>;
   }
 
   if (smallScreen && !main) {
+    console.log('smallScreenContainer rendered');
     return <SmallScreenContainer {...rest}>{children}</SmallScreenContainer>;
   }
 
