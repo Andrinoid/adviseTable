d2e3af9060a670a71409877ac0bc1f0db1f2b832
Andri <andri@advise.is>
2024-06-05 15:28:09 +0000

    avoid 0 screen measurement

--- src/components/Layout/hooks/useSmallScreen.js
+++ src/components/Layout/hooks/useSmallScreen.js
@@ -3,5 +3,5 @@ import { useViewportSize } from '@mantine/hooks';
 export default function useSmallScreen() {
   const { width } = useViewportSize();
 
-  return width < 600;
+  return width < 600 && width > 0;
 }
