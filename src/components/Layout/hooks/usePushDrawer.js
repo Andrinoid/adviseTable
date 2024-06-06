d14820455e3d96217c70ba64766cabc7be7bb7bb
Franklyn Kleiton <franklynkleiton18@gmail.com>
2024-06-06 13:47:04 -0300

    update pushdrawer

--- src/components/Layout/hooks/usePushDrawer.js
+++ src/components/Layout/hooks/usePushDrawer.js
@@ -2,15 +2,13 @@ import React from 'react';
 import produce from 'immer';
 
 import useLayout from './useLayout';
-import useClear from './useClear';
 
 const usePushDrawer = () => {
   const { setDrawers, siders } = useLayout();
-  const clear = useClear();
 
   return (component, root = false) => {
     if (siders.length > 0 && root) {
-      clear();
+      setDrawers([]);
       setTimeout(() => {
         setDrawers((previous) =>
           produce(previous, (draft) => {
