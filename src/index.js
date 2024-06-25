4eabefc30ec118a7b5d4e09338731487c99e3c3a
Andri <andri@advise.is>
2024-06-25 13:53:31 +0000

    remove absolute position on mobile

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import ExampleSimple from './ExampleSimple';
 import { RootStyle } from './global.style';
 // import SortExample from './SortExample';
-import App from './components/Grid/App';
+// import App from './components/Grid/App';
 import { createRoot } from 'react-dom/client';
 // import App from "./components/ContextMenu/App";
-// import App from './components/Layout/App';
+import App from './components/Layout/App';
 
 const Application = () => (
   <>
